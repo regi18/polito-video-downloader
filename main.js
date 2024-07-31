fe172d2b6cb7573a5b2b10d2ce23407b06a7cc5a
Simone Carletti <regi18@outlook.it>
2024-07-31 18:12:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-javascript:(function() {
+javascript:(async function() {
     var baseUrl="https://didattica.polito.it/portal/pls/portal/";
+
     /* creazione attributo replaceAll per le stringhe */
     String.prototype.replaceAll = function(search, replacement) {
         var target = this;
@@ -7,24 +8,40 @@ javascript:(function() {
     };
 
     console.log("Getting links...");
+
     /* prendo tutto il contetnuo della pagina */
     var htmlContent=document.documentElement.innerHTML;
     var reg=/href="(sviluppo\.videolezioni\.vis.*lez=[0-9]*)">/gi;
     var matches, output = [];
+
     /* inserisco i link dentro output */
     while (matches = reg.exec(htmlContent)) {
         output.push(matches[1].replaceAll("&amp;", "&"));
     }
-    /* stampo i link trovati */
-    document.documentElement.innerHTML="";
-    output.forEach(function(item, index){
 
-        console.log((index+1) +" - "+ item);
-        /* se riesco a ottenere il contenuto dell'url eseguo function(data) */
-        $.get(baseUrl+item).success(function(data){
-            var dlLink = data.match(/https:\/\/video\.polito\.it\/dl\/.*\.mp4/gi);
-            /* prendo il primo match */
-            document.write("<a href=\""+dlLink[0]+"\">"+dlLink[0].split("/").pop()+"</a><br>\n");
-        });
-    });
+    // Function to fetch video URL
+    const fetchVideoUrl = async (url) => {
+        try {
+            const response = await fetch(baseUrl + url);
+            const data = await response.text();
+            const dlLink = data.match(/https:\/\/video\.polito\.it\/dl\/.*\.mp4/gi);
+
+            if (dlLink) return dlLink[0]; 
+        } 
+        catch (error) {
+        }
+
+        return null;
+    };
+
+    // Fetch and display video URLs
+    const urls = [];
+    for (let [index, item] of output.entries()) {
+        console.log(`${index + 1} - ${item}`);
+        const videoUrl = await fetchVideoUrl(item);
+        if (videoUrl) urls.push(videoUrl);
+    }
+
+    document.documentElement.innerHTML = `Found ${urls.length} videos. You can use the following with JDownloader: <br><br>`;
+    document.documentElement.innerHTML += urls.join(",");
 })()
